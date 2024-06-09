33738adbc669b763119ccf417eb1dc4c9c43fa27
Simo Hellgrén <simo.hellgren@relexsolutions.com>
2024-06-09 17:31:44 +0300

    implement tag merging

--- filetags/src/models2/tag.py
+++ filetags/src/models2/tag.py
@@ -52,4 +52,51 @@ class Tag:
             yield Tag(self.name)
 
         for child in self.children:
-            yield Tag(self.name, list(child.paths()))
+            for path in child.paths():
+                yield Tag(self.name, [path])
+
+    def lowest_common_node(self, other: Self):
+        """Finds the lower common node between self and other.
+        Other must not be branching and self and other must start at same node.
+
+        returns the lowest common node as well as the remainder of other
+        """
+        # if other has no more childen, done
+        if not other.children:
+            return self, None
+
+        # get the next node to check
+        (next_other,) = other.children
+
+        next_self = next(
+            (child for child in self.children if child.name == next_other.name), None
+        )
+
+        # if no next_self is found, self is the LCN
+        if not next_self:
+            return self, next_other
+
+        # otherwise recurse
+        return next_self.lowest_common_node(next_other)
+
+    def merge(self, other: Self):
+        """Merges other to self
+        This (as well as lowest_common_node) can probably be simplified or partially combined,
+        but this shall do for now.
+        """
+
+        # generate non-branching paths of other
+        paths = other.paths()
+        for path in paths:
+            # look for a shared root
+            root = self.find(path)
+
+            # if not found, add path at self
+            if not root:
+                self.children.append(path)
+                continue
+
+            # otherwise, find the lowest common parent and add the remainder to its children
+            parent, remainder = root.lowest_common_node(path)
+            if remainder:
+                parent.children.append(remainder)
