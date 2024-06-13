798df5eac57c3f194d639f7caf87286ef78dedc1
Simo Hellgrén <simo.hellgren@relexsolutions.com>
2024-06-13 13:40:42 +0300

    add another test for json conversion

--- filetags/tests/test_json.py
+++ filetags/tests/test_json.py
@@ -3,6 +3,17 @@ from filetags.src.models2.node import Node
 from filetags.src.models2.vault import Vault, VaultJSONEncoder
 
 
+def nodes_equal(a: Node, b: Node):
+    """helper function to recursively check equality of nodes"""
+    if a.value != b.value:
+        return False
+
+    if len(a.children) != len(b.children):
+        return False
+
+    return all(nodes_equal(x, y) for x, y in zip(a.children, b.children))
+
+
 def test_node(nodes: list[Node]):
     [n1, n2, n3, n4, n5, n6, n7, n8] = nodes
 
@@ -18,6 +29,18 @@ def test_vault(vault: Vault):
     assert result
     assert isinstance(result, list)
     assert len(result) == 2
+
+
+def test_vault2(vault: Vault):
+    """Converts vault to json and back, and checks that the result is the same"""
+    json_string = json.dumps(vault, cls=VaultJSONEncoder)
+
+    vault2 = Vault.from_json(json.loads(json_string))
+
+    node1 = Node("root", vault._entries)
+    node2 = Node("root", vault2._entries)
+
+    assert nodes_equal(node1, node2)
 
 
 def test_encoder(vault: Vault, nodes: list[Node]):
