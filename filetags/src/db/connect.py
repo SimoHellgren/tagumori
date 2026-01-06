7dc047ba2426b2d169f57f377650e0141d1b3491
Simo Hellgrén <simo.hellgren@relexsolutions.com>
2026-01-06 20:10:27 +0200

    typo

--- filetags/src/db/connect.py
+++ filetags/src/db/connect.py
@@ -6,5 +6,5 @@ DEFAULT_VAULT_PATH = Path("vault.db")
 
 def get_vault(path: Path = DEFAULT_VAULT_PATH) -> sqlite3.Connection:
     conn = sqlite3.connect(path)
-    conn.execute("PRAGMA foreign_key = ON")
+    conn.execute("PRAGMA foreign_keys = ON")
     return conn
