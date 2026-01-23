cab30dc83aa3a8487472a86c913a040e1d2881ff
Simo Hellgrén <hellgrensimo@gmail.com>
2026-01-23 21:30:58 +0200

    vault backup

--- filetags/commands/db.py
+++ filetags/commands/db.py
@@ -1,14 +1,16 @@
+import sqlite3
+from datetime import datetime
 from pathlib import Path
-from sqlite3 import Connection
 
 import click
 
+from filetags.commands.context import LazyVault
 from filetags.db.init import init_db
 
 
 @click.group(help="Database management")
 @click.pass_obj
-def db(vault: Connection):
+def db(vault: LazyVault):
     pass
 
 
@@ -21,3 +23,38 @@ def init(filepath: Path):
     else:
         init_db(filepath)
         click.echo(f"{filepath} created.")
+
+
+@db.command(help="Create a backup of vault.")
+@click.argument("dest", type=click.Path(path_type=Path), required=False)
+@click.option(
+    "-d",
+    "--dir",
+    "directory",
+    type=click.Path(
+        path_type=Path,
+        file_okay=False,
+    ),
+    default=Path("."),
+    help="Directory to save backup into.",
+)
+@click.pass_obj
+def backup(vault: LazyVault, dest: Path, directory: Path):
+    if dest is None:
+        timestamp = datetime.now().strftime("%Y%m%d-%H%M%S")
+        backup_name = f"{vault._path.stem}-{timestamp}.db"
+
+    else:
+        backup_name = dest
+
+    backup_path = directory / backup_name
+
+    if backup_path.exists():
+        click.confirm(f"{backup_path} already exists. Overwrite?", abort=True)
+        backup_path.unlink()
+
+    with vault as source:
+        with sqlite3.connect(backup_path) as destination:
+            source.backup(destination)
+
+    click.echo(f"Backup created: {backup_path}")
